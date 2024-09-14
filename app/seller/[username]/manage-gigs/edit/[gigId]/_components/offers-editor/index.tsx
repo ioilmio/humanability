7b88435089feb4e1beaaf8c6eb0720edf7757b61
quadro <illuminato.salvatore@gmail.com>
2024-09-14 16:56:51 +0200

    edit gig: fix tabs

--- app/seller/[username]/manage-gigs/edit/[gigId]/_components/offers-editor/index.tsx
+++ app/seller/[username]/manage-gigs/edit/[gigId]/_components/offers-editor/index.tsx
@@ -1,5 +1,6 @@
 import { Id } from "@/convex/_generated/dataModel";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import * as Tabs from '@radix-ui/react-tabs';
 // import {
 //   Tooltip,
 //   TooltipContent,
@@ -29,22 +30,21 @@ export const OffersEditor = ({ gigId }: OffersEditorProps) => {
   const standardOffer = offers.find((offer) => offer.tier === "Standard");
   const premiumOffer = offers.find((offer) => offer.tier === "Premium");
   return (
-    <Tabs defaultValue="Basic" className="w-[400px]"
->
-      <TabsList className="w-fit">
-        <TabsTrigger value="Basic" >Basic</TabsTrigger>
-        <TabsTrigger value="Standard">Standard</TabsTrigger>
-        <TabsTrigger value="Premium">Premium</TabsTrigger>
-      </TabsList>
-      <TabsContent value="Basic">
+    <Tabs.Root defaultValue="Basic" className="w-1/2">
+      <Tabs.List className="flex h-10 items-center justify-between rounded-md bg-muted p-1 text-muted-foreground">
+        <Tabs.Trigger className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-[10%] py-1.5 text-md font-medium ring-offset-foreground transition-all focus-visible:outline-none focus-visible:ring-6 focus-visible:ring-foreground focus-visible:ring-offset-6 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary  data-[state=active]:text-foreground data-[state=active]:shadow-sm" value="Basic" >Basic</Tabs.Trigger>
+        <Tabs.Trigger className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-[10%] py-1.5 text-md font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-6 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary  data-[state=active]:text-foreground data-[state=active]:shadow-sm" value="Standard">Standard</Tabs.Trigger>
+        <Tabs.Trigger className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-[10%] py-1.5 text-md font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-6 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary  data-[state=active]:text-foreground data-[state=active]:shadow-sm" value="Premium">Premium</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="Basic">
         <ContentEditor gigId={gigId} offer={basicOffer} tier="Basic" />
-      </TabsContent>
-      <TabsContent value="Standard">
+      </Tabs.Content>
+      <Tabs.Content value="Standard">
         <ContentEditor gigId={gigId} offer={standardOffer} tier="Standard" />
-      </TabsContent>
-      <TabsContent value="Premium">
+      </Tabs.Content>
+      <Tabs.Content value="Premium">
         <ContentEditor gigId={gigId} offer={premiumOffer} tier="Premium" />
-      </TabsContent>
-    </Tabs>
+      </Tabs.Content>
+    </Tabs.Root>
   );
 };
